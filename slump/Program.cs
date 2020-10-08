fe86a2e28cd613253a54cbecdb44c925b4c0993c
Marcus <marcus.isaksson@elev.ga.ntig.se>
2020-10-08 15:06:05 +0200

    lade upp grundstruktur

--- slump/Program.cs
+++ slump/Program.cs
@@ -6,7 +6,26 @@ namespace slump
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int hpPlayer1 = 100;
+            int hpPlayer2 = 100;
+            bool playersAlive = true;
+
+            Console.WriteLine(hpPlayer1 + hpPlayer2 + "HP total");
+            Console.WriteLine("FIGHT TO THE DEATH!");
+            Console.ReadLine();
+
+            while (playersAlive == true)
+            {
+
+
+                if (hpPlayer1 < 1)
+                {
+                    playersAlive = false;
+                    Console.WriteLine("Player 2 wins!");
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
