876d782d13dc26ae6684b7fd606b5b82460f56a7
Marcus <marcus.isaksson@elev.ga.ntig.se>
2020-10-12 09:23:56 +0200

    debuggade, lade till draw som resultat

--- slump/Program.cs
+++ slump/Program.cs
@@ -8,29 +8,37 @@ namespace slump
         {
             int hpPlayer1 = 100;
             int hpPlayer2 = 100;
-            int healthPool = hpPlayer1 + hpPlayer2;
+
             int currentRound = 0;
             bool playersAlive = true;
 
             Random generator = new Random();
-            int damage = generator.Next(20);
-            int counterDamage = generator.Next(20);
 
-            Console.WriteLine(healthPool + "HP total");
+            Console.WriteLine("Trump and Biden have 200 HP in total");
             Console.WriteLine("FIGHT TO THE DEATH!");
             Console.ReadLine();
 
             while (playersAlive == true)
             {
                 currentRound++;
+                int damage = generator.Next(20);
+                int counterDamage = generator.Next(20);
                 hpPlayer2 = hpPlayer2 - damage;
                 hpPlayer1 = hpPlayer1 - counterDamage;
+                int healthPool = hpPlayer1 + hpPlayer2;
 
                 Console.WriteLine("Round " + currentRound + " Biden deals " + damage + " damage! Trump has " + hpPlayer2 + " health left!");
                 Console.WriteLine("Round " + currentRound + " Trump deals " + counterDamage + " damage! Biden has " + hpPlayer1 + " health left!");
                 Console.ReadLine();
 
-                if (hpPlayer1 < 1)
+
+                if (healthPool < 1)
+                {
+                    playersAlive = false;
+                    Console.WriteLine("Draw! Noone wins!");
+                    Console.ReadLine();
+                }
+                else if (hpPlayer1 < 1)
                 {
                     playersAlive = false;
                     Console.WriteLine("Trump wins!");
@@ -40,12 +48,6 @@ namespace slump
                 {
                     playersAlive = false;
                     Console.WriteLine("Biden wins!");
-                    Console.ReadLine();
-                }
-                if (healthPool < 1)
-                {
-                    playersAlive = false;
-                    Console.WriteLine("Draw noone wins!");
                     Console.ReadLine();
                 }
             }
