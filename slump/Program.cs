a761fc0310639a81da8de4d96c94629464e525d1
Marcus <marcus.isaksson@elev.ga.ntig.se>
2020-10-12 09:30:37 +0200

    Lade till kommentarer

--- slump/Program.cs
+++ slump/Program.cs
@@ -9,6 +9,7 @@ namespace slump
             int hpPlayer1 = 100;
             int hpPlayer2 = 100;
 
+            // currentRound skriver upp vilken runda som körs (främst för att jag skulle kunna debugga)
             int currentRound = 0;
             bool playersAlive = true;
 
@@ -20,18 +21,22 @@ namespace slump
 
             while (playersAlive == true)
             {
+                // currentRound++ ändrar vilken runda som körs efter varje gång propgrammet loopar
                 currentRound++;
+                // Här definieras mängden startliv - mängden damage för att få ut resulterande liv
                 int damage = generator.Next(20);
                 int counterDamage = generator.Next(20);
                 hpPlayer2 = hpPlayer2 - damage;
                 hpPlayer1 = hpPlayer1 - counterDamage;
+                // Används för att skapa "draw" som resultat ifall båda "dör"
                 int healthPool = hpPlayer1 + hpPlayer2;
 
+                // Grafisk/textbaserad respons till användaren.
                 Console.WriteLine("Round " + currentRound + " Biden deals " + damage + " damage! Trump has " + hpPlayer2 + " health left!");
                 Console.WriteLine("Round " + currentRound + " Trump deals " + counterDamage + " damage! Biden has " + hpPlayer1 + " health left!");
                 Console.ReadLine();
 
-
+                // Om healthpool är mindre än 1 innebär det att båda är döda >> ingen vinner.
                 if (healthPool < 1)
                 {
                     playersAlive = false;
