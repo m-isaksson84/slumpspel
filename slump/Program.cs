77950f2df22c89c22955aebf8892088bfdb7247e
Marcus <marcus.isaksson@elev.ga.ntig.se>
2020-10-12 08:52:47 +0200

    lade till random int och textmeddelanden

--- slump/Program.cs
+++ slump/Program.cs
@@ -8,7 +8,12 @@ namespace slump
         {
             int hpPlayer1 = 100;
             int hpPlayer2 = 100;
+            int currentRound = 0;
             bool playersAlive = true;
+
+            Random generator = new Random();
+            int damage = generator.Next(10);
+            int counterDamage = generator.Next(10);
 
             Console.WriteLine(hpPlayer1 + hpPlayer2 + "HP total");
             Console.WriteLine("FIGHT TO THE DEATH!");
@@ -16,14 +21,31 @@ namespace slump
 
             while (playersAlive == true)
             {
+                hpPlayer2 = hpPlayer2 - damage;
+                Console.Clear();
+                currentRound++;
+                Console.WriteLine("Round " + currentRound + " Biden deals " + damage + " damage! Trump has " + hpPlayer2 + " health left!");
+                Console.ReadLine();
+                Console.Clear();
 
+                hpPlayer1 = hpPlayer1 - counterDamage;
+                Console.Clear();
+                Console.WriteLine("Round " + currentRound + " Trump deals " + counterDamage + " damage! Biden has " + hpPlayer2 + " health left!");
+                Console.ReadLine();
+                Console.Clear();
 
                 if (hpPlayer1 < 1)
                 {
                     playersAlive = false;
-                    Console.WriteLine("Player 2 wins!");
+                    Console.WriteLine("Trump wins!");
+                    Console.ReadLine();
                 }
-
+                else if (hpPlayer2 < 1)
+                {
+                    playersAlive = false;
+                    Console.WriteLine("Biden wins!");
+                    Console.ReadLine();
+                }
                 Console.ReadLine();
             }
         }
